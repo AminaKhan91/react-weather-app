e7b6686b3eb86cf7a4ab6046a76d6a00dbbe3361
AminaKhan91 <96996241+AminaKhan91@users.noreply.github.com>
2022-04-28 17:33:14 +0100

    Added Bootstrap

--- src/App.js
+++ src/App.js
@@ -3,27 +3,29 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1> Weather App </h1>
-      <footer>
-        This project was coded by{" "}
-        <a
-          href="https://gracious-euclid-fb1db5.netlify.app/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Amina Khan{" "}
-        </a>{" "}
-        and is
-        <a
-          href="
+      <div className="container">
+        <h1> Weather App </h1>
+        <footer>
+          This project was coded by{" "}
+          <a
+            href="https://gracious-euclid-fb1db5.netlify.app/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Amina Khan{" "}
+          </a>{" "}
+          and is
+          <a
+            href="
           https://github.com/AminaKhan91/react-weather-app"
-          target="_blank"
-          rel="noreferrer"
-        >
-          {" "}
-          open sourced on GitHub
-        </a>
-      </footer>
+            target="_blank"
+            rel="noreferrer"
+          >
+            {" "}
+            open sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
