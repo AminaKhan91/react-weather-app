93254c6c24a8f8f0b0f75a50bcabf3df0263479f
AminaKhan91 <96996241+AminaKhan91@users.noreply.github.com>
2022-04-28 17:59:27 +0100

    Added some HTML

---
+++ src/Weather.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import "./Weather.css";
+
+export default function Weather() {
+  return (
+    <div className="Weather">
+      <p> Thursday, 28 April 2022, 17:57 </p>
+      <p>overcast clouds </p>
+      Hello from Weather{" "}
+    </div>
+  );
+}
